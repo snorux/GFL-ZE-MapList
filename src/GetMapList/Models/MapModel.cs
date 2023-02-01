210a609d57ceb3dc17f4de0f7e718000359514a4
Snowy <34697265+snooooowy@users.noreply.github.com>
2023-02-01 19:41:30 +0800

    Create MapModel

---
+++ src/GetMapList/Models/MapModel.cs
@@ -0,0 +1,9 @@
+﻿namespace GetMapList.Models
+{
+    internal class MapModel
+    {
+        public string MapName { get; set; }
+        public bool IsMoreThan150MB { get; set; }
+        public long FileSize { get; set; }
+    }
+}
